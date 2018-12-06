b4c0696fde419daccb57661fad1aa766fc34d9cf
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-12-06 16:10:41 +0000

    Update docs after building Travis build 12 of HBClab/atlascorr
    The docs were built from the branch 'master' against the commit
    1db13b087073c21e6497413f5a7986ab30fba704.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/HBClab/atlascorr/jobs/464436922.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post18+g0efdd9b',
+    VERSION: '0.post19+g1db13b0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
